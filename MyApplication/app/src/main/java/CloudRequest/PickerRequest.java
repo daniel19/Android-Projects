1a47ac2dd39c4bb763b96687d897c1082d6d1c1c
MacDeveloper <daniel19brown@gmail.com>
2015-01-10 15:21:06 -0600

    Added code to read from GCS

---
+++ MyApplication/app/src/main/java/CloudRequest/PickerRequest.java
@@ -0,0 +1,95 @@
+package CloudRequest;
+
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.dbi.myapplication.main;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+/**
+ * This class will use the Google Cloud Json API to request
+ * objects stored in the cloud.
+ */
+public class PickerRequest extends AsyncTask<String, Void, String> {
+    public static final String API_KEY = "AIzaSyDKxepSnwG891YRUtkfnfTDe5VoyHmH08s";
+    public static final String MAIN_URL = "https://www.googleapis.com/storage/v1";
+    public static final String GET_URL = "/b/json_parts/o/list.json";
+
+    JSONObject jsonResponse;
+    Context context;
+    main activity;
+    public PickerRequest(Context context, main activity){
+        jsonResponse = null;
+        this.context = context;
+        this.activity = activity;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+       String data = "";
+        try{
+           data = download(params[0]);
+       }catch(IOException ex){
+           Log.d("PICKER", ex.getMessage());
+       }
+        return data;
+    }
+    @Override
+    protected void onPostExecute(String result){
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        activity.setListView(jsonResponse);
+    }
+
+
+    private String download(String url) throws IOException{
+        InputStream inputStream = null;
+        String result = "FAILED";
+        URL local_url = new URL(url);
+        HttpsURLConnection connection = (HttpsURLConnection) local_url.openConnection();
+        try{
+            connection.setReadTimeout(10000); //10 second timeout
+            connection.setConnectTimeout(15000); //15 second timeout
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+
+            connection.connect();
+            Log.d("PICKER", url);
+            Log.d("PICKER", "Response Code: " + connection.getResponseCode());
+            inputStream = connection.getInputStream();
+
+            jsonResponse = convertData(inputStream);
+            result = "success";
+        }catch(IOException|JSONException ex) {
+            result += ex.getMessage();
+        }finally{
+            connection.disconnect();
+        }
+        return result;
+    }
+
+    private JSONObject convertData(InputStream inputStream) throws IOException, JSONException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+        StringBuilder builder = new StringBuilder();
+        String input;
+        while((input = reader.readLine()) != null){
+            builder.append(input);
+        }
+        return new JSONObject(builder.toString());
+    }
+
+    public JSONObject getJsonResponse(){return jsonResponse;}
+
+}
--- MyApplication/app/src/main/java/com/dbi/myapplication/main.java
+++ MyApplication/app/src/main/java/com/dbi/myapplication/main.java
@@ -1,9 +1,19 @@
 package com.dbi.myapplication;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import CloudRequest.PickerRequest;
 
 /**
  * main class will be the front page controller.
@@ -14,6 +24,49 @@ public class main extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*final ListView listview = (ListView) findViewById(R.id.partsList);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);*/
+        //Load Json file from GCS
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()){
+            //Create PickerRequest object
+            PickerRequest pickerRequest = new PickerRequest(getApplicationContext(), this);
+            //pickerRequest.execute(PickerRequest.MAIN_URL + PickerRequest.GET_URL + "?key=" +PickerRequest.API_KEY);
+            pickerRequest.execute("https://storage.googleapis.com/json_parts/list.json");
+        }else{
+            Toast.makeText(getApplicationContext(), "Not connected to a network.", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    public void setListView(JSONObject jsonResponse) {
+        //Get CPU List
+        try {
+            if(jsonResponse != null) {
+                //A dictionary with a JSONArray array of arrays.
+               JSONArray cpuList = (JSONArray) jsonResponse.get("cpu");
+
+            }else{
+                Toast.makeText(getApplicationContext(), "No Data was returned.", Toast.LENGTH_SHORT).show();
+            }
+
+        }catch(JSONException ex){
+            Toast.makeText(getApplicationContext(), "A parsing error occurred.", Toast.LENGTH_LONG).show();
+        }
     }
 
 
